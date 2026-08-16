6cc784b5c5675ee3bb82dce4e4f4e816499fcc9b
Ryan Palmer <www.ryan.palmer@hotmail.com>
2026-08-16 18:14:21 +1000

    Apply decimal stretched-mode scale factor

--- client/src/net/runelite/client/plugins/stretchedmode/StretchedModePlugin.java
+++ client/src/net/runelite/client/plugins/stretchedmode/StretchedModePlugin.java
@@ -95,7 +95,7 @@ public class StretchedModePlugin extends Plugin
 			client.setStretchedFast(config.increasedPerformance());
 			client.setStretchedIntegerScaling(config.integerScaling());
 			client.setStretchedKeepAspectRatio(config.keepAspectRatio());
-			client.setScalingFactor(config.scaling() * 100);
+			client.setScalingFactor((int) Math.round(config.scaling() * 100.0));
 			client.invalidateStretching(true);
 		});
 	}
